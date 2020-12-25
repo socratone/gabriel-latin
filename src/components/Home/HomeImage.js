6a056a5e7de841e3bc3d2bbbafa2472e38285a20
socratone <gim2origin@gmail.com>
2020-12-25 14:28:25 +0900

    홈 이미지 컴포넌트로 분리

---
+++ src/components/Home/HomeImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HomeImage.module.scss';
+
+const HomeImage = () => {
+  return (  
+    <section className={styles.imageWrap}>
+      <div className={styles.image} style={{ backgroundImage: `url(https://cdn.pixabay.com/photo/2017/09/07/16/50/acropolis-2725918_1280.jpg)`}}></div>
+    </section>
+  );
+}
+ 
+export default HomeImage;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import HomeImage from '../Home/HomeImage';
 import styles from './Home.module.scss';
 
 const Home = () => {
   return (  
-    <div>
-      <div className={styles.backImage} style={{ backgroundImage: `url(https://cdn.pixabay.com/photo/2016/08/22/18/11/sky-1612521_1280.jpg)`}}></div>
-    </div>
+    <main>
+      <HomeImage />
+    </main>
   );
 }
  
