c48de6b87fb4db1b52c84ed9f991e11a9b27e11d
socratone <gim2origin@gmail.com>
2020-12-25 14:44:19 +0900

    HomeImage 색 어둡게 조정

--- src/components/Home/HomeImage.js
+++ src/components/Home/HomeImage.js
@@ -5,6 +5,7 @@ const HomeImage = () => {
   return (  
     <section className={styles.imageWrap}>
       <div className={styles.image} style={{ backgroundImage: `url(https://cdn.pixabay.com/photo/2017/09/07/16/50/acropolis-2725918_1280.jpg)`}}></div>
+      <div className={styles.imageColor}></div>
     </section>
   );
 }
