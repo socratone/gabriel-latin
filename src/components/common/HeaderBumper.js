76058de46e978dcbf4c4f44146902cc962f887c6
socratone <gim2origin@gmail.com>
2020-12-27 02:29:51 +0900

    헤더에 라인 추가

--- src/components/common/HeaderBumper.js
+++ src/components/common/HeaderBumper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const HeaderBumper = styled.section`
   flex-shrink: 0;
   height: 60px;
+  border-bottom: 1px solid gainsboro;
 `;
 
 export default HeaderBumper;
