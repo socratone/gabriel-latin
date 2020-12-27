205c3a58438d30792d216715406bb6aebbee8bdd
socratone <gim2origin@gmail.com>
2020-12-28 02:09:37 +0900

    RightNavItem에 디자인 적용

--- src/components/nav/RightNavItem.js
+++ src/components/nav/RightNavItem.js
@@ -3,15 +3,20 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { toggleMenuButton } from '../../store/nav';
 
+const style = {
+  color: '#707070',
+  marginBottom: '8px'
+}
+
 const RightNavItem = ({ to, children }) => {
   const dispatch = useDispatch();
-  
+
   const handleClick = () => {
     dispatch(toggleMenuButton());
   };
 
   return (  
-    <Link to={to} onClick={handleClick}>
+    <Link to={to} onClick={handleClick} style={style}>
       {children}
     </Link>
   );
