9ad081979d0564f9e5af1467f6e08d681472cef1
socratone <gim2origin@gmail.com>
2021-01-04 13:34:16 +0900

    Nav 배경색

--- src/components/common/HeaderBumper.js
+++ src/components/common/HeaderBumper.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const HeaderBumper = styled.section`
   flex-shrink: 0;
   height: 60px;
-  border-bottom: 1px solid gainsboro;
 `;
 
 export default HeaderBumper;
