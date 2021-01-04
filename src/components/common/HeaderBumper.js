7c4a429a26c9df82bc908880e77545899110e2aa
socratone <gim2origin@gmail.com>
2021-01-04 15:25:32 +0900

    Nav 디자인 수정

--- src/components/common/HeaderBumper.js
+++ src/components/common/HeaderBumper.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
+const HEADER_HEIGHT_MOBILE = '40px'
+
 const HeaderBumper = styled.section`
   flex-shrink: 0;
   height: 60px;
+
+  @media (max-width: 800px) {
+    height: ${HEADER_HEIGHT_MOBILE};
+  }
 `;
 
 export default HeaderBumper;
