7430d8e60cb716d25fe11c4e6e05ad41342f1bb4
socratone <gim2origin@gmail.com>
2021-01-14 09:16:40 +0900

    RegularCourses 이미지 수정

--- src/components/Courses/Title.js
+++ src/components/Courses/Title.js
@@ -10,7 +10,7 @@ const Title = styled.h3`
   ${props => props.main && css`
     font-size: 2rem;
     font-weight: 700;
-    margin-top: 2rem;
+    margin-top: 3rem;
   `}
     
   ${props => props.big && css`
