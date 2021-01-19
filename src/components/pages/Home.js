11b2db2dc01b4eed596d35bc1c9b82e17ede0411
socratone <gim2origin@gmail.com>
2021-01-19 18:28:44 +0900

    홈에 목업 글자 추가

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomeImage from '../Home/HomeImage';
+import PageFrame from '../common/PageFrame';
 import styles from './Home.module.scss';
 
 const Home = () => {
   return (  
     <main>
       <HomeImage />
+      <PageFrame>
+        <p>안녕하세요!</p>
+        <p>안녕하세요!</p>
+        <p>안녕하세요!</p>
+        <p>안녕하세요!</p>
+        <p>안녕하세요!</p>
+        <p>안녕하세요!</p>
+        <p>안녕하세요!</p>
+      </PageFrame>
     </main>
   );
 }
